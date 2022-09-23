a197b00ad3d8e930900e8b8a26aad13f7f12126c
Grace <olabodephoeb@gmail.com>
2022-09-23 06:48:24 -0700

    Task solutions

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+'''Handling lazy_matrix_mul function to multiply two matrices'''
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    '''Multiplying two matrices using numpy
+    Args:
+        m_a (list of lists of int/float): The fist matrix to be multiplied
+        m_b (list of lists of int/float): The second matrix to be multiplied
+    '''
+
+    tan = np.matmul(m_a, m_b)
+    return tan
