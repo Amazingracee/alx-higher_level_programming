495bfb7988044502ec760c01ed39a06b59c45c22
Grace <olabodephoeb@gmail.com>
2022-09-27 19:04:59 -0700

    Solutions

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+'''Defines a method that returns an object represented by a JSON string'''
+import json
+
+
+def from_json_string(my_str):
+    '''Returns an object representated by a JSON string
+    Args:
+        my_str (any): The object to be represented
+    '''
+    return json.loads(my_str)
