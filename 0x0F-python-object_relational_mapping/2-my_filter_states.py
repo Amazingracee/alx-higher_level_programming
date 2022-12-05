b7ea4cee7aeb21403f0ecf188cbb5a2ce07300cc
Grace <olabodephoeb@gmail.com>
2022-12-05 02:31:06 +0000

    pycodestyle lint fix

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-
+""""A script that lists all states from the database hbtn_0e_0_usa"""
 from sys import argv
 import MySQLdb
-
+# db = MySQLdb.connect(mysql username=sys.argv[0],mysql
+# password=sys.argv[1],database name=sys.argv[3],localhost=3306)
 if __name__ == '__main__':
-    conn = MySQLdb.connect(port=3306, user=argv[1], passwd=argv[2], db=argv[3]
-            )
+    conn = MySQLdb.connect(port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE '%s'" %(argv[4]))
+    cur.execute("SELECT * FROM states WHERE name LIKE '%s'" % (argv[4]))
     states = cur.fetchall()
     for state in states:
         print(state)
