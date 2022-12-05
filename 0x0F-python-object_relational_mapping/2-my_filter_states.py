0d334baff2859e94a58de4fa1f478f039f83f257
Grace <olabodephoeb@gmail.com>
2022-12-05 02:33:58 +0000

    pycodestyle lint fix

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,7 +7,7 @@ import MySQLdb
 if __name__ == '__main__':
     conn = MySQLdb.connect(port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE '%s'" % (argv[4]))
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '%s'" % (argv[4]))
     states = cur.fetchall()
     for state in states:
         print(state)
