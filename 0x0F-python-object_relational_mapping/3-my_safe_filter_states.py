c45e80dd886d1122d70b38eff168b2f72533c177
Grace <olabodephoeb@gmail.com>
2022-12-05 02:59:41 +0000

    fix

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,13 +1,25 @@
 #!/usr/bin/python3
+"""
+This script takes in an argument and
+displays all values in the states
+where `name` matches the argument
+from the database `hbtn_0e_0_usa`.
+This time the script is safe from
+MySQL injections!
+"""
 
 from sys import argv
 import MySQLdb
 
 if __name__ == '__main__':
-    conn = MySQLdb.connect(port=3306, user=argv[1], passwd=argv[2], db=argv[3]
-            )
+    """
+    Access to the database and get the states
+    from the database.
+    """
+    conn = MySQLdb.connect(port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name = %(name)s", {'name': argv[4]})
+    cur.execute("SELECT * FROM states WHERE name = %(name)s\
+            ORDER BY states.id ASC", {'name': argv[4]})
     states = cur.fetchall()
     for state in states:
         print(state)
