34a5b58ed4385688a57bc126d7249cba88089bed
Grace <olabodephoeb@gmail.com>
2022-12-05 03:34:42 +0000

    All cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
+"""
+This script  takes in the name of a state
+as an argument and lists all cities of that
+state, using the database `hbtn_0e_4_usa`.
+"""
 
 from sys import argv
 import MySQLdb
 
 if __name__ == '__main__':
+    """
+    Access to the database and get the cities
+    from the database.
+    """
+
     conn = MySQLdb.connect(port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT cities.name FROM cities JOIN states ON\
+    cur.execute("SELECT cities.id,cities.name FROM cities JOIN states ON\
             states.id = cities.state_id WHERE\
             states.name = %(state_name)s ORDER\
             BY cities.id ASC", {'state_name': argv[4]})
     cities = cur.fetchall()
-    lists = []
-    for city in cities:
-        lists += city
-    print(", ".join(lists))
+    if cities is not None:
+        print(", ".join([city[1] for city in cities]))
     cur.close()
     conn.close()
