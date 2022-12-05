8b65f17caf71b39e10f61d364b9023e02253b1fb
Grace <olabodephoeb@gmail.com>
2022-12-05 02:45:38 +0000

    add order by

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,7 +7,7 @@ import MySQLdb
 if __name__ == '__main__':
     conn = MySQLdb.connect(port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name = '" + (argv[4]) +"'")
+    cur.execute("SELECT * FROM states WHERE name = '" + (argv[4]) +"' order by states.id")
     states = cur.fetchall()
     for state in states:
         print(state)
