4794af0f4e151ee0bcac1b6794f7d3d2a600c0b6
Grace <olabodephoeb@gmail.com>
2022-12-05 01:48:38 +0000

    pycodestyle error fixing

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-
+"""
+A script that lists all states with a name starting with
+N (upper N) from the database hbtn_0e_0_usa
+"""
 from sys import argv
 import MySQLdb
-
+# db = MySQLdb.connect(mysql username=sys.argv[0],mysql
+# password=sys.argv[1],database name=sys.argv[3],localhost=3306)
 if __name__ == '__main__':
-    conn = MySQLdb.connect(port=3306, user=argv[1], passwd=argv[2], db=argv[3]
-            )
+    conn = MySQLdb.connect(port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id  ASC;")
     states = cur.fetchall()
