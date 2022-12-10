20c156b1e44cc33ad62e354d4747c540e3cea3b9
Grace <olabodephoeb@gmail.com>
2022-12-10 02:41:57 +0000

    fixing requirement errors

--- 0x0F-python-object_relational_mapping/6-model_state.py
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import sys
 from model_state import Base, State
--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-
-""" This script lists all State objects from the database hbtn_0e_6_usa """
+"""
+This script lists all State objects
+from the database `hbtn_0e_6_usa`.
+"""
 
 from sys import argv
-from model_state import Base, State
-from sqlalchemy import create-engine
+from model_state import State, Base
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
@@ -12,12 +14,14 @@ if __name__ == "__main__":
     Access to the database and get the states
     from the database.
     """
-    conn = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
         argv[1], argv[2], argv[3])
 
-    engine = create_engine(conn)
+    engine = create_engine(db_url)
     Session = sessionmaker(bind=engine)
 
     session = Session()
+
     for instance in session.query(State).order_by(State.id):
         print('{0}: {1}'.format(instance.id, instance.name))
